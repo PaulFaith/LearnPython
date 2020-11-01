8a9c8ecbf2bc17f9064d5eafc90e4b9cc9005c7d
PaulFaith <62285801+PaulFaith@users.noreply.github.com>
2020-11-01 06:42:06 +0100

    Update myjson.py
    First two methods created, read and write.

--- json/myjson.py
+++ json/myjson.py
@@ -1,11 +1,13 @@
 import json
 
 class Myjson:
-  def __init__(self,file):
-    self._file=file
-  
+  def __init__(self,file,new):
+    self._file = file
+    self._new = new
   def read(self):
+    with open(self._file) as doc:
+      data = json.load(doc)
     
   def write(self):
-  
-  def add(self):
+    with open(self.file,"w") as doc:
+      json.dump(self.new,doc)
