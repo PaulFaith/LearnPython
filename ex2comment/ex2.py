4d05c36adfea18b9a178741fad7bd69304876ac2
PaulFaith <62285801+PaulFaith@users.noreply.github.com>
2020-11-03 03:04:41 +0100

    Create ex2.py
    Simple comments Code!!

---
+++ ex2comment/ex2.py
@@ -0,0 +1,7 @@
+#JUST REMEMBER HASHTAG I FOR COMMENTING SOMETHING!!!! 
+
+print("Everybody wants to comment something") #OCTOTHORPE sounds strange but sexy. ;)
+
+#This comment is not a scream comment, no more uppercase.
+
+print("RATITA, SAL DE AHÍ RATITA QUIERO VERTE LA COLITA...")
