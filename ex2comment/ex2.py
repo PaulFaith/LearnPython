c79aca9a0014932043cb88d48280aba221a2b4d1
PaulFaith <62285801+PaulFaith@users.noreply.github.com>
2020-11-03 03:08:11 +0100

    Update ex2.py

--- ex2comment/ex2.py
+++ ex2comment/ex2.py
@@ -5,3 +5,6 @@ print("Everybody wants to comment something") #OCTOTHORPE sounds strange but sex
 #This comment is not a scream comment, no more uppercase.
 
 print("RATITA, SAL DE AHÍ RATITA QUIERO VERTE LA COLITA...")
+
+print("No one is going to play rayuela during my teach... Not really sure")
+print("# # # Love you kitty, love you hashtag, love you ...")
