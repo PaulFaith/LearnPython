bccf300cf649982ebc61c8d7e02d347bc1fddd52
PaulFaith <62285801+PaulFaith@users.noreply.github.com>
2020-11-03 02:56:57 +0100

    Create ex1.py
    First file for teach and creating muscular memory. VIVA EL PULPO PAUL!!

---
+++ ex1/ex1.py
@@ -0,0 +1,15 @@
+#As an old sabio said practice, practice and practice. GENIUS se hace GENIUS also nace but also if you born GENIUS and practice then you will be GENIUS square if not your 
+exponent number will be between 0 and 1 that means GENIUS will suddenly disapear!! GO, GO, GOOOOO...!! 
+
+print("Hello world!! I'm Paul")
+print("As an old sabio said PRACTICE!!")
+print("Ok another print that always is remembering me...")
+print("No sapces between print an the pharentesis")
+print("Love you mom")
+print("Love you wife")
+print("Love you my funky son, GG :v....")
+print("Tell me your birthday and i will sing you a song")
+print("My ands don't want to stop")
+print("oK I KNOW IT'S HANDS NO ANDS..")
+print("ands is a new word a mix between hands and ants")
+print("Like hands made of ants.... O.o")
