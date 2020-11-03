2b8100e63fb00e0c736f7ce45e92712511714ba0
PaulFaith <62285801+PaulFaith@users.noreply.github.com>
2020-11-03 03:22:04 +0100

    Create ex3.py
    That was tooooooo many prints""" and math symbols.
    
    GREAT!!!!

---
+++ ex3math/ex3.py
@@ -0,0 +1,25 @@
+#SOOO lets play with sing symbols and sing!!
+
+print("Time to count, time to math: ")
+print("Blue lies", 24 + 48 / 7)
+print("Pink lies", 123 + 23 - 50 * 4 % 2)
+
+print("The first time i saw you was: ")
+
+print(3 + 2 - 1 + 35 *2 % 5 - 4 / 2 + 3)
+print("Crazy result i know!!")
+
+print("Important question 3 + 4 < 4 - 3")
+
+print(3 + 4 < 4 - 3)
+
+print("Blue pill 3 + 4 ",3 + 4)
+print("Red pill 4 - 3", 4 - 3)
+
+print("Welcome back")
+
+print("This is my last...")
+
+print("Is it logic that 5 is less -2", 5 < -2)
+print("Greater or equal", 5 >= -2)
+print("lESS OR EQUAL", 5 <= -2)
