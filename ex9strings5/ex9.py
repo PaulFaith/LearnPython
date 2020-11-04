43972bfd72ee3e86be800b813123f88682665387
PaulFaith <jupafeguz@gmail.com>
2020-11-04 01:01:47 +0000

    ex9 done!! BABY!!

---
+++ ex9strings5/ex9.py
@@ -0,0 +1,14 @@
+days = "Lun Mar Mie Jue Vie Sab"
+months = "Ene\nFeb\nMar\nAbr\nMay\nJun\nJul\nAgo\nSep\nOct\nNov\nDic"
+
+print("The days of the week are: ", days)
+print("And the monkeys are: ", months)
+
+print (""" La cucharacha.
+La chumacarra....
+Ya se quiere echar a volar....
+Porqué le ladra??
+Poruqe... uaracha..
+Cocacola con vinar..
+heheheheehehhe!!!
+""")
