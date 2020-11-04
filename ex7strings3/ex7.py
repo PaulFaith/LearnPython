27832e4afe535da4f59b9f37b4f7c32b8c0fe2e4
PaulFaith <jupafeguz@gmail.com>
2020-11-04 00:54:42 +0000

    ex7 & ex8 ready!!!

---
+++ ex7strings3/ex7.py
@@ -0,0 +1,10 @@
+print("Todas las mañanas cuando me levanto")
+print(", tengo la pirula más dura que un {} jejeje.".format('canto'))
+print("." * 50)
+
+l1 =  "Y"
+l2 = "E"
+l3 = "S"
+l4 = ".*º^º*."
+
+print(l1 + l2 + l3, l1==' ', l4 * 2)
---
+++ ex8string4/ex8.py
@@ -0,0 +1,7 @@
+formatter = "{} {} {} {}"
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("ones", "twos", "threes", "f¡safaoiraiurus"))
+
+print(formatter.format(True, True, False, True))
+print(formatter.format(formatter.format(1,2,3,4),formatter,formatter,formatter.format("peaxe", "muevelo", "gozalo", "Oh mama!!")))
+print("I'm ", "your future perfect ", "coder ", "live with that bro. jijijijij!!!")
