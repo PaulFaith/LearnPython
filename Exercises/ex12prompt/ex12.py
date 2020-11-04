d7cd0746a64c5890be98c5abf8de9a7a883d9d01
PaulFaith <jupafeguz@gmail.com>
2020-11-04 01:46:24 +0000

    Exercise 12 DONE!!!

---
+++ Exercises/ex12prompt/ex12.py
@@ -0,0 +1,7 @@
+#Asking adding string inside the input function.
+
+age = input("How old are you?")
+height = input("How tall are you")
+weight = input("How much do you wieght?")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy... AMAZING!!")
