102477e9d4f24f64ed68032d33dc944aed6ad8e7
PaulFaith <jupafeguz@gmail.com>
2020-11-04 01:34:38 +0000

    More and more excercises!!!

---
+++ Exercises/ex11ask/ex11.py
@@ -0,0 +1,6 @@
+print("Tell me your name: ", end=' ')
+name = input()
+print("And your age, tell me your fabouluse age: ", end=' ')
+age = input()
+
+print(f"So, your name is {name}, and your are {age} years old. NICE!!!)
