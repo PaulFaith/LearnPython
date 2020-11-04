4a34192f5bdf213b77ea3e0109bf9b97c9b08f50
PaulFaith <jupafeguz@gmail.com>
2020-11-04 00:20:04 +0000

    New excercise 5 and 6 about strings & stuff.

--- ex4variables/ex4.py
+++ ex4variables/ex4.py
@@ -1,1 +1,13 @@
-print("myfriend")
+home = 3
+rooms_in_homes = 5
+livers = 7
+house_in_use = 1
+pets = 4
+house_not_in_use = house_in_use - home
+not_animal_users = livers - pets
+total_rooms = rooms_in_homes * home
+
+print("I have: ", home, "homes.")
+print("Each of my homes has: ", rooms_in_homes, "rooms.")
+print("We love pets, that's why we have: ", pets, "pets.")
+print("So actually we are: ", not_animal_users, "peoples living togheter.\nWe don't use ", abs(house_not_in_use), "houses, but we have a total of ", total_rooms, "rooms." )
---
+++ ex5strings/ex5.py
@@ -0,0 +1,15 @@
+my_name = "Paul Faith"
+my_cat = "Turmi"
+my_age = 34
+my_foot = 44
+my_hand = 33
+my_hair = "brown"
+my_ring = "blue MaGiC eye"
+
+print(f"Hello, I'm {my_age}")
+print(f"{my_cat} came to me 5 years ago")
+print(f"{my_cat} has the same color of my ring, My ring is a: {my_ring}.")
+
+total = my_age + my_foot + my_hand
+
+print(f"The sum of my age, my foot and my hands are: {total + total} YES!!")
