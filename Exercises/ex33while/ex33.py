3dca92d8b8a9fb1f670b6d34bd9aae087eae4587
PaulFaith <jupafeguz@gmail.com>
2020-11-14 02:33:03 +0000

    Enough for today

---
+++ Exercises/ex33while/ex33.py
@@ -0,0 +1,17 @@
+i = 0
+numeros = []
+
+while i < 10 :
+  if i == 1 :
+    print(f"Tengo {i} fruta.\n")
+  else :
+    print(f"Tengo {i} frutas.\n")
+  
+  print("Pero si agrego una fruta, entonces ahora tengo:\n")
+  i+=1
+  if i == 1 :
+    print(f"Ahora Tengo {i} fruta.\n")
+  else :
+    print(f"Ahora Tengo {i} frutas.\n")
+
+print("ADIOS AMIGOS!!")
